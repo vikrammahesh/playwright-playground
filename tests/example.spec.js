daac88ba40ad75e964c0144ae196949b995bdbe6
Mahesh Vikram <vikrammahesh@gmail.com>
2023-08-01 06:38:20 +0000

    updated import

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test } = require('../lamdatest-setup')
+const { expect } = require('@playwright/test')
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
